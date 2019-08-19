a0bd4d99a3d22f91d33d51b402ad48435b5fb25a
github username <github email address>
2019-08-19 10:47:21 -0400

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,19 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render 'birds/index.html.erb'
+   @birds = Bird.all
+   render json: @birds
+   #render json: ['As','well','as','arrays']
   end
+
+  def plain
+    render plain: "I am a plain language"
+  end
+
+
+  def show
+    bird = Bird.find(params[:id])
+    #render json: bird, only: [:name, :species]
+    render plain: "I am bird number #{bird.id}, and my name is #{bird.name}"
+  end
+
 end
